5e0bbb1f0c153306c56f8e1620cafcef46ad41ed
Daniel Sanchez Ceinos <daniel.ceinos@gmail.com>
2019-06-14 09:42:45 +0200

    Format user agent

--- useragent/src/main/java/com/danielceinos/useragent/UserAgentInterceptor.kt
+++ useragent/src/main/java/com/danielceinos/useragent/UserAgentInterceptor.kt
@@ -1,20 +1,45 @@
 package com.danielceinos.useragent
 
 import android.content.Context
+import android.content.pm.PackageManager
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class UserAgentInterceptor(val context: Context) : Interceptor {
 
-    override fun intercept(chain: Interceptor.Chain): Response {
+class UserAgentInterceptor(private val context: Context) : Interceptor {
 
-        val builder = chain.request().newBuilder()
-
-        builder.header("User-Agent", buildUserAgent(context))
+    private val userAgent: String by lazy {
+        buildUserAgent(context)
     }
 
-    private fun buildUserAgent(context: Context):String {
-        val versionName = context.packageManager.getPackageInfo(this.context.packageName, 0).versionName
-        val versionCode = this.context.packageManager.getPackageInfo(this.context.packageName, 0).versionCode
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder = chain.request().newBuilder()
+        builder.header("User-Agent", userAgent)
+        return chain.proceed(builder.build())
+    }
+
+    private fun buildUserAgent(context: Context): String {
+        val versionName = try {
+            context.packageManager.getPackageInfo(this.context.packageName, 0).versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            "nameNotFound"
+        }
+        val versionCode = try {
+            context.packageManager.getPackageInfo(this.context.packageName, 0).versionCode.toString()
+        } catch (e: PackageManager.NameNotFoundException) {
+            "versionCodeNotFound"
+        }
+
+        val appName = try {
+            context.packageManager.getApplicationInfo(this.context.packageName, 0)
+        } catch (e: PackageManager.NameNotFoundException) {
+            "appNameNotFount"
+        }
+
+        val installerName = context.packageManager.getInstallerPackageName(context.packageName)
+
+        val agent = System.getProperty("http.agent")
+
+        return "$appName / $versionName($versionCode); $installerName; $agent"
     }
 }
